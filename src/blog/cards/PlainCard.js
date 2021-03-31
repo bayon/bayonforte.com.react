553b52dfccecf351384531ce6d63129c2f8b52b3
Bayon Forte <bayon@forteworks.com>
2021-03-31 17:39:53 -0400

    hide show functionality for details.

--- src/blog/cards/PlainCard.js
+++ src/blog/cards/PlainCard.js
@@ -1,21 +1,31 @@
 import { Typography } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import "./card.css";
+
+
 
 const PlainCard = (props) => {
     console.log('PlainCard props:',props)
+    const [seeDetails,setSeeDetails] = useState(false); 
+
   return (
     <div  className="card-plain">
       <Typography variant="h5" component="h2">
         {props.user.fullName}
       </Typography>
 
-      <Typography variant="body2" component="p" color="textSecondary">
-      {props.user.email}
-        
-        
-      </Typography>
-      <button onClick={() => {props.details(props.user)}} >details</button>
+  
+      {/* <button onClick={() => {props.details(props.user)}} >details</button> */}
+      <button onClick={() => {setSeeDetails(!seeDetails)} } >{seeDetails ? "hide" : "details" }</button>
+      {seeDetails && 
+      (
+        <Typography variant="body2" component="p" color="textSecondary">
+        {props.user.email}
+          
+          
+        </Typography>
+      )
+      }
     </div>
   );
 };
