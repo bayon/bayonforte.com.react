f3d9d957a3ad9d85f997e8e27c4ebb85e0fc66ce
Bayon Forte <bayon@forteworks.com>
2021-04-05 09:53:44 -0400

    ok

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-echo "hi "
-git fetch || git pull origin mmaster || npm run-script build || echo "ok now mv build to correct location."
+echo "hi first run git fetch, enter password then run this script. sudo sh deploy_script.sh "
+git pull origin mmaster || 
+npm run-script build || 
+echo "ok now mv build to correct location." ||
+rm -rf /var/www/bayonforte.com/build  ||
+mv build /var/www/bayonforte.com/
