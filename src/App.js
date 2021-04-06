69e719e42e479a7e3ad85d426b53edc3d41b8ebe
Bayon Forte <bayon@forteworks.com>
2021-04-06 07:03:25 -0400

    added missing dependencies and everything works. Now stripe payment needs to be in a logical ui spot.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-//
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { Provider } from "react-redux";
 import "./App.css";
-//stripe dependencies:
 import HomePage from "./blog/pages/HomePage";
 import Navigation from "./Navigation";
 import store from "./redux/store";
-//const stripePromise = loadStripe('pk_live_c03o8lC0VMl6y5eBpZ1eov45');  //test versus live
 const stripePromise = loadStripe("pk_test_7aHY16H2I0thccZMQJIDUNpi");
 
 function App(props) {
