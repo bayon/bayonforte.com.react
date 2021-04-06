5967522160443007cc5ca3b6304cdc3167f542da
Bayon Forte <bayon@forteworks.com>
2021-04-06 06:57:35 -0400

    added checklist of stripe items needed.

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import React from "react";
 import { Provider } from "react-redux";
 import "./App.css";
+//stripe dependencies:
+import HomePage from "./blog/pages/HomePage";
 import Navigation from "./Navigation";
 import store from "./redux/store";
-
+//const stripePromise = loadStripe('pk_live_c03o8lC0VMl6y5eBpZ1eov45');  //test versus live
+const stripePromise = loadStripe("pk_test_7aHY16H2I0thccZMQJIDUNpi");
 
 function App(props) {
-
   return (
     <Provider store={store}>
-       
-      <div className="App">
-        <React.Fragment>
-          <CssBaseline />
-          <Container maxWidth="lg">
-            <Navigation />
-          </Container>
-        </React.Fragment>
-      </div>
-       
+      <Elements stripe={stripePromise}>
+        <div className="App">
+          <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="lg">
+              <Navigation />
+            </Container>
+          </React.Fragment>
+          <HomePage />
+        </div>
+      </Elements>
     </Provider>
   );
 }
---
+++ src/blog/pages/HomePage.js
@@ -0,0 +1,153 @@
+// MUI Components
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+// Util imports
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+// stripe
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import axios from 'axios';
+import React, { useState } from 'react';
+// Custom Components
+import CardInput from '../components/CardInput';
+//prod_JFUtg4zMnpq3lg 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 500,
+    margin: '35vh auto',
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'flex-start',
+  },
+  div: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignContent: 'flex-start',
+    justifyContent: 'space-between',
+  },
+  button: {
+    margin: '2em auto 1em',
+  },
+});
+
+function HomePage() {
+  const classes = useStyles();
+  // State
+  const [email, setEmail] = useState('');
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmitPay = async (event) => {
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    const res = await axios.post('http://localhost:4000/pay', {email: email});
+
+    const clientSecret = res.data['client_secret'];
+
+    const result = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          email: email,
+        },
+      },
+    });
+
+    if (result.error) {
+      // Show error to your customer (e.g., insufficient funds)
+      console.log(result.error.message);
+    } else {
+      // The payment has been processed!
+      if (result.paymentIntent.status === 'succeeded') {
+        console.log('Money is in the bank!');
+        // Show a success message to your customer
+        // There's a risk of the customer closing the window before callback
+        // execution. Set up a webhook or plugin to listen for the
+        // payment_intent.succeeded event that handles any business critical
+        // post-payment actions.
+      }
+    }
+  };
+
+
+  const handleSubmitSub = async (event) => {
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    const result = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+      billing_details: {
+        email: email,
+      },
+    });
+
+    if (result.error) {
+      console.log(result.error.message);
+    } else {
+      const res = await axios.post('http://localhost:4000/sub', {'payment_method': result.paymentMethod.id, 'email': email});
+      // eslint-disable-next-line camelcase
+      const {client_secret, status} = res.data;
+
+      if (status === 'requires_action') {
+        stripe.confirmCardPayment(client_secret).then(function(result) {
+          if (result.error) {
+            console.log('There was an issue!');
+            console.log(result.error);
+            // Display error message in your UI.
+            // The card was declined (i.e. insufficient funds, card has expired, etc)
+          } else {
+            console.log('You got the money 1!');
+            // Show a success message to your customer
+          }
+        });
+      } else {
+        console.log('You got the money 2!');
+        // No additional information was needed
+        // Show a success message to your customer
+      }
+    }
+  };
+
+
+  return (
+    <Card className={classes.root}>
+      <CardContent className={classes.content}>
+        <TextField
+          label='Email'
+          id='outlined-email-input'
+          helperText={`Email you'll recive updates and receipts on`}
+          margin='normal'
+          variant='outlined'
+          type='email'
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          fullWidth
+        />
+        <CardInput />
+        <div className={classes.div}>
+          <Button variant="contained" color="primary" className={classes.button} onClick={handleSubmitPay}>
+            Pay
+          </Button>
+          <Button variant="contained" color="primary" className={classes.button} onClick={handleSubmitSub}>
+            Subscription
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default HomePage;
