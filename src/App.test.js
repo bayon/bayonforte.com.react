7e410711992e7820ff6e865809a9b8b4befcd25f
Bayon Forte <bayon@forteworks.com>
2021-04-09 10:45:38 -0400

    creating basic render tests for the pages.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+ 
+ 
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App />);
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    screen.debug();
+    //screen.findAllByText('Resume');
+
+  });
 });
