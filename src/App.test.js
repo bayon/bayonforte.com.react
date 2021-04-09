989ae204f0e2b5d9a37c7cae19eb1cae35e3a31c
Bayon Forte <bayon@forteworks.com>
2021-04-09 11:13:42 -0400

    add some testing templates ie for mock api call.

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from './App';
  
- 
+ // REFERENCE: https://www.robinwieruch.de/react-testing-library
 describe('App', () => {
   test('renders App component', () => {
     render(<App />);
