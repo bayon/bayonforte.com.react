9ba77c4112fb76ad26eb34d41933e25c3730319f
Bayon Forte <bayon@forteworks.com>
2021-04-10 10:48:56 -0400

    added basic sorting UI controls.

--- src/blog/pages/SearchPage.js
+++ src/blog/pages/SearchPage.js
@@ -16,16 +16,13 @@ const SearchPage = (props) => {
   const [sortId, setSortId] = useState(false);
   const [sortZip, setSortZip] = useState(false);
   const [sortState, setSortState] = useState(false);
-  const [noSort, setNoSort] = useState(true);
+  const [noSort, setNoSort] = useState(false);
 
   console.log("Search Page reached ...props:", props);
   useEffect(() => {
     dispatch(authAction.allUsers())
       .then(async (result) => {
         console.log("ALL USERS RESULTS:", result);
-        setSortName(false);
-        setSortEmail(false);
-        setSortId(false);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -33,31 +30,7 @@ const SearchPage = (props) => {
     return <div>not authorized.</div>;
   }
 
-  // const getAllUsers = () => {
-  //   dispatch(authAction.allUsers())
-  //     .then(async (result) => {
-  //       console.log("result:", result);
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
-
-  const sortByColor = (list) => {
-    list
-      .sort((a, b) => (a.color > b.color ? 1 : -1))
-      .map((item, i) => console.log("item:" + item.color + "index" + i));
-  };
-  const sortBySize = (list) => {
-    list
-      .sort((a, b) => (a.size > b.size ? 1 : -1))
-      .map((item, i) => console.log("item:" + item.size + "index" + i));
-  };
-  const list = [
-    { color: "white", size: "XXL" },
-    { color: "red", size: "XL" },
-    { color: "black", size: "M" },
-  ];
-  sortByColor(list);
-  sortBySize(list);
+  
 
   const sortByFullName = (users) => {
     users
@@ -66,41 +39,65 @@ const SearchPage = (props) => {
         return <PlainCard user={user}></PlainCard>;
       });
   };
+  const clearSortOptions = () => {
+    //SET SORT OPTIONS HERE:
+    setSortName(false);
+    setSortEmail(false);
+    setSortId(false);
+    setNoSort(false);
+  };
+  const setSortOption = (e) => {
+    console.log(e.target.value);
+    const key = e.target.value;
+    clearSortOptions();
+    switch (key) {
+      case "id":
+        setSortId(true);
+        break;
+      case "name":
+        setSortName(true);
+        break;
+      case "email":
+        setSortEmail(true);
+        break;
+      default:
+        setNoSort(true);
+        break;
+    }
+  };
 
   return (
     <div>
       <Paper>
-        <div>Users List</div>
         <div>
-          <ul>
-            <li>
-              DETAILS
-              <ul>
-                <li>See Contact Details and General Info of Individual Post</li>
-                <li>Is the poster's info verified.</li>
-                <li>Map of Posters location.</li>
-                <li>Distance Poster is willing to travel.</li>
-                <li>Image of Poster</li>
-              </ul>
-            </li>
-            <li>
-              ACTIONS:
-              <ul>
-                <li>
-                  Send Poster a request to :
-                  <ul>
-                    <li>Get an Estimate</li>
-                    <li>Collaborate on a job.</li>
-                    <li>Leave comments on Experience or Service.</li>
-                  </ul>
-                </li>
-                <li>Search and Filter the list </li>
-              </ul>
-            </li>
-          </ul>
+          <span>
+            Sort Options:
+            <input
+              type="radio"
+              id="name"
+              name="sortOption"
+              value="name"
+              onChange={setSortOption}
+            />
+            <label for="name">Name</label>
+            <input
+              type="radio"
+              id="email"
+              name="sortOption"
+              value="email"
+              onChange={setSortOption}
+            />
+            <label for="email">Email</label>
+            <input
+              type="radio"
+              id="id"
+              name="sortOption"
+              value="id"
+              onChange={setSortOption}
+            />
+            <label for="id">Id</label>
+          </span>
         </div>
-
-        
 
         {haveUsers &&
           sortName &&
@@ -132,6 +129,34 @@ const SearchPage = (props) => {
             return <PlainCard user={user}></PlainCard>;
           })}
 
+        <div>
+          <ul>
+            <li>
+              DETAILS
+              <ul>
+                <li>See Contact Details and General Info of Individual Post</li>
+                <li>Is the poster's info verified.</li>
+                <li>Map of Posters location.</li>
+                <li>Distance Poster is willing to travel.</li>
+                <li>Image of Poster</li>
+              </ul>
+            </li>
+            <li>
+              ACTIONS:
+              <ul>
+                <li>
+                  Send Poster a request to :
+                  <ul>
+                    <li>Get an Estimate</li>
+                    <li>Collaborate on a job.</li>
+                    <li>Leave comments on Experience or Service.</li>
+                  </ul>
+                </li>
+                <li>Search and Filter the list </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </Paper>
     </div>
   );
