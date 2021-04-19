424020ffaaf8ad2d3601e70e087dd7d5c5ca8e36
Bayon Forte <bayon@forteworks.com>
2021-04-19 09:21:19 -0400

    add work links

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -5,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import PropTypes from "prop-types";
 import React from "react";
-
+import '../App.css';
 const bayon =
   "https://media-exp1.licdn.com/dms/image/C4D03AQHXzD5ZUmbpSw/profile-displayphoto-shrink_200_200/0/1516574446401?e=1619049600&v=beta&t=4hI89lgoF7rS7uomD0_kLlEtVIIRHJn6Dobs06yP5UE";
 
@@ -34,7 +34,7 @@ export default function Footer(props) {
   const classes = useStyles();
 
   return (
-    <footer className={classes.footer}>
+    <footer className={classes.footer} style={{marginBottom:"100px"}}>
       <Container maxWidth="lg">
         <Grid container>
           <Grid item xs={12} sm={4}>
@@ -44,19 +44,21 @@ export default function Footer(props) {
                   href="tel:8122670592"
                   style={{ color: "#222", textDecoration: "none" }}
                 >
-                  (812) 267-0592<Icon>phone</Icon>
+                  (812) 267-0592<Icon className="appIcon">phone</Icon>
                 </a>
               </div>
               <div style={{ marginTop: 15 }}>
                 <a
+                className="appAnchor"
                   href="mailto:forteworks@gmail.com"
                   style={{ color: "#222", textDecoration: "none" }}
                 >
-                  forteworks@gmail.com<Icon>email</Icon>
+                  forteworks@gmail.com<Icon className="appIcon">email</Icon>
                 </a>
               </div>
               <div style={{ marginTop: 15 }}>
                 <a
+                className="appAnchor"
                   href="http://www.bayonforte.com/Forte_2021_gmail.pdf"
                   target="blank"
                   style={{ textDecoration: "none", color: "#222",fontWeight:"bold" }}
@@ -85,23 +87,14 @@ export default function Footer(props) {
           </Grid>
 
           <Grid item xs={12} sm={4}>
+            <p>Work:</p>
             <div style={{ textAlign: "center", marginTop: "20px" }}>
-              <img
-                src={bayon}
-                alt="..."
-                style={{ borderRadius: "25px", height: "100px" }}
-              />
+              <a className="appAnchor" href="http://www.servewerx.com" target="blank">servewerx.com  <Icon className="appIcon">web</Icon> </a>
             </div>
-            <div>
-              <a
-                href="http://www.bayonforte.com/Forte_2021_gmail.pdf"
-                target="blank"
-                style={{ textDecoration: "none", color: "#222" }}
-                download
-              >
-                Resume
-              </a>
+            <div style={{ textAlign: "center", marginTop: "20px" }}>
+              <a className="appAnchor" href="http://www.blue-cottage-remodeling.com" target="blank">blue-cottage-remodeling.com <Icon className="appIcon">web</Icon> </a>
             </div>
+           
           </Grid>
         </Grid>
       </Container>
